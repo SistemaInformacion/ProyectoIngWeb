97bc4fe3ee08e8e68f7e2a8eda34d9c8fe5d8146
jairobernal <jairobernal2000@hotmail.com>
2014-07-28 10:57:12 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Jairiño</title>
+        <script>
+            alert("Hola");
+        </script>
+    </head>
+    <body>
+        <div style=" with:100%; position:absolute;letf:50%; top:50%; text-align: center; ">
+            Ingenieria Web
+        </div>
+    </body>
+</html>
